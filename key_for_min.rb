71783b4ad056a6104b94bf014ed86d912ef7c55f
Zachary Durr <zdurr22@gmail.com>
2020-10-08 22:47:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  smallest = name_hash[0, 0]
+  smallest = name_hash[0]
   i = 0
   while i < name_hash.length
     if name_hash[i] < smallest
