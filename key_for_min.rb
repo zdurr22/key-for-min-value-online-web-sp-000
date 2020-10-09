df1c931f4c9f6ba29e4929f9548e10828a62a566
Zachary Durr <zdurr22@gmail.com>
2020-10-09 00:50:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,14 +9,13 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       array << value
     end
-    min_num = array[0]
-    array.each do |value|
-      min_num = value if value < min_num
-    end
-    name_hash.each do |key, value|
-      if min_num == value
-        return key
-      end
+  min_value = array[0]
+  name_hash.each do |key, value|
+    min_value = value if value < min_value
+  end
+  name_hash.each do |key, value|
+    if value == min_value
+      return key
     end
   end
 end
